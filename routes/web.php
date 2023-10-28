d141b899d758b19145d963431b0f7b9acb146d8f
sirluks <sirluks@gmail.com>
2023-10-28 14:43:52 -0300

    retornando aos trabalhos

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +28,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chart', function () {
+        
+        /* $teste = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => '',
+            'max_tokens' => 500,
+        ]);
+        dd($teste); */
+        return 'Lucas';
+    });
 });
 
 require __DIR__.'/auth.php';
